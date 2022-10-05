ac328443810164e897213260b7dd2d63de348697
Adityaweb23 <74668650+Adityaweb23@users.noreply.github.com>
2022-10-05 14:08:50 +0530

    first commit

--- src/card.jsx
+++ src/card.jsx
@@ -3,7 +3,9 @@ import { NavLink } from 'react-router-dom';
 const Card = (props) => {
     return (
         <>
-            <div className="col-md-4 col-10 mx-auto gy-4">
+            <div className="col-md-6 col-10 mx-auto gy-4">
+            <div className="col-sm-6 col-10 mx-auto gy-4">
+            
 
                 <div className="card">
                     <img src={props.image} className="card-img-top" alt="no image found" />
@@ -11,6 +13,7 @@ const Card = (props) => {
                         <h5 className="card-title">{props.title}</h5>
                         <p className="card-text">This is one of the best programming language</p>
                         <NavLink to="/" className="btn btn-primary">Go somewhere</NavLink>
+                    </div>
                     </div>
                 </div>
             </div>
